7ec3497b477cbd38d20417ddde8b6e05830aaaa7
Limwngur <ovg07047@naver.com>
2024-11-11 14:53:21 +0900

    feat: springdoc 의존성 추가 및 path 설정

---
+++ src/main/java/com/iiie/server/config/SpringDocConfig.java
@@ -0,0 +1,14 @@
+package com.iiie.server.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public GroupedOpenApi api(){
+        return GroupedOpenApi.builder().group("api").pathsToExclude("/api/**").build();
+    }
+}
